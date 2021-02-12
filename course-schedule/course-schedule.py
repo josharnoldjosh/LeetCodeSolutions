349a1af2b4c7795ff96691fb4e550e266cebf815
Josh Arnold <josharnold997@gmail.com>
2021-02-12 09:06:47 -0800

    Time: 96 ms (77.60%), Space: 17.4 MB (25.41%) - LeetHub

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -1,25 +1,24 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-                        
-        graph = collections.defaultdict(list)        
-        
-        for a, b in prerequisites:
-            graph[a].append(b)
-            
-        visited = {i:0 for i in range(0, numCourses)}
-        
-        def dfs(i):
-            if visited[i] == 1: return True
-            if visited[i] == -1: return False
-            visited[i] = -1
-            for j in graph[i]:
-                if not dfs(j):
-                    return False
-            visited[i] = 1
-            return True
-        
-        for i in range(numCourses):
-            if not dfs(i):
-                return False
-            
-        return True
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+                
+        
+        def dfs(course):  
+            if seen[course] == 1: return True
+            if seen[course] == -1: return False
+            seen[course] = -1
+            for next_course in graph[course]:
+                if not dfs(next_course):
+                    return False
+            seen[course] = 1
+            return True
+        
+                                        
+        # Create graph
+        graph = collections.defaultdict(list)        
+        for b, a in prerequisites: graph[a] += [b]
+            
+        # Seen
+        seen = collections.defaultdict(int)
+                        
+        # Dfs on each course        
+        return all([dfs(course) for course in range(numCourses)])
