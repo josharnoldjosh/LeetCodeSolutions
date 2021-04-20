01c8e305ebf9736168ff68e29bd875de9a73902e
Josh Arnold <josharnold997@gmail.com>
2021-04-20 08:52:39 -0700

    Time: 52 ms (52.86%), Space: 15.8 MB (17.62%) - LeetHub

--- filling-bookcase-shelves/filling-bookcase-shelves.py
+++ filling-bookcase-shelves/filling-bookcase-shelves.py
@@ -1,13 +1,24 @@
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelf_width: int) -> int:        
         
-        @functools.lru_cache(maxsize=None)
-        def recurse(i, width, height):            
-            if width < 0: return float('inf')
-            if i >= len(books): return height
-            w, h = books[i]
-            add_to_cur_row = recurse(i+1, width-w, max(height, h))
-            start_new_row = height + recurse(i+1, shelf_width-w, h)
-            return min(add_to_cur_row, start_new_row)
+        @functools.lru_cache(None)
+        def recurse(idx, row_width, row_height):
+            if row_width > shelf_width: return float('inf')
+            if idx >= len(books): return row_height
+            w, h = books[idx]
+            this_row = recurse(idx+1, row_width+w, max(row_height, h))
+            next_row = row_height + recurse(idx+1, w, h)
+            return min(this_row, next_row)
         
-        return recurse(0, shelf_width, 0)
+        return recurse(0, 0, 0)
+        
+        
+        
+    
+"""
+Actions:
+- place book on current shelf
+- place book on next shelf
+
+- ask for forgiveness rather than permission
+"""
