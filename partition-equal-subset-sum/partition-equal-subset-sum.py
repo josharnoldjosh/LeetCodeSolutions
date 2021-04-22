83f0f1add989bd6ebcbf3041aa1085ddb08e737a
Josh Arnold <josharnold997@gmail.com>
2021-04-22 08:56:58 -0700

    Time: 2096 ms (36.38%), Space: 17.4 MB (56.01%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.py
+++ partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,13 +1,16 @@
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
+    def canPartition(self, nums: List[int]) -> bool:        
+        return 0 in functools.reduce(lambda dp, num: {num - i for i in dp} | {num + i for i in dp}, nums, {0})
+        
+#         dp = {0}
+#         for num in nums:
+#             dp = {num - i for i in dp} | {num + i for i in dp}        
+#         return 0 in dp
+    
+    
+"""
         dp = {0}
         for num in nums:
             dp = {num - i for i in dp} | {num + i for i in dp}        
         return 0 in dp
-    
-    
-"""
-- We have an array
-- Elements have "value"
-- Given a function, in this case, sum(x), we want to minimize the absolute difference between two subsets
 """
