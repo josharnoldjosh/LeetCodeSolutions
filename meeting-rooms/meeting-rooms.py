181c651eec32b9430c4f57a72bf3943cba002f5a
Josh Arnold <josharnold997@gmail.com>
2021-05-05 07:54:23 -0700

    Time: 80 ms (23.81%), Space: 17.5 MB (9.09%) - LeetHub

--- meeting-rooms/meeting-rooms.py
+++ meeting-rooms/meeting-rooms.py
@@ -1,12 +1,7 @@
 class Solution:
-    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        
-        result = []
-        
-        for i in sorted(intervals, key=lambda x: x[0]):
-            if not result or result[-1][1] <= i[0]:
-                result.append(i)
-            else:
-                result[-1][1] = max(result[-1][1], i[1])
-                
-        return len(result) == len(intervals)
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:        
+        intervals.sort()
+        return all(
+            i[1] <= j[0]
+            for i, j in zip(intervals[:-1], intervals[1:])
+        )
