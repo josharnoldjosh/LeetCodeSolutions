dbd4d3f0272effbab256171c4fee7d1353a5f866
Josh Arnold <josharnold997@gmail.com>
2021-05-07 11:04:06 -0700

    Time: 1092 ms (6.15%), Space: 14.2 MB (93.32%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,61 +1,26 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-                        
-        queue = collections.deque([])        
-        window = set()
+    def lengthOfLongestSubstring(self, s: str) -> int:        
+        
+        i, j = 0, 1
+        
         result = 0
-        pointer = 0
         
-        while pointer < len(s):
-            
-            c = s[pointer]
-            
-            if c in window:
-                while queue:
-                    prev = queue.popleft()
-                    window.remove(prev)
-                    if prev == c:
-                        break
+        s = s.replace(" ", "Œ")
+        
+        while j <= len(s)+1:                               
+            count = collections.Counter(s[i:j]).most_common(1)                        
+            if not count:
+                j += 1
+                i = j-1            
+                result = max(result, len(s[i:j]))
+            elif count[0][-1] <= 1:
+                result = max(result, len(s[i:j]))
+                j += 1
+            else:                
+                i += 1
                 
-            window.add(c)
-            queue.append(c)
-            result = max(result, len(window))
-            pointer += 1
-            
         return result
-                
-            
-            
-        
-        
     
 """
-- try every substring via recursion
-- counter, most common
-- record length...
-
-T(n) = 2*T(n -1) + n
-= 2^(n-1)*n = 2^n
-
-----
-
-set expansion?
-- start at each character
-- try to expand right?
-- stop if existing character is found?
-
-aaaaaaaaaa -> O(n)
-
-abcdefg -> n^2 runtime
-
-----
-
-sliding window
-
-start adding to a queue,
-- when existing value is encountered, pop queue until value gone and add new value?
-- use a set to make it fast
--> O(n)??
-
-abca
+T(n) = 2 * T(n - 1) -> 2^n
 """
