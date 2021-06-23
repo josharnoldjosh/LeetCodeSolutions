b3da7fe10c6dafe9c1d402c7c74459a44641d720
Josh Arnold <dev@josharnold.me>
2021-06-23 07:22:17 -0700

    Time: 32 ms (51.33%), Space: 14.3 MB (41.64%) - LeetHub

--- thousand-separator/thousand-separator.py
+++ thousand-separator/thousand-separator.py
@@ -1,4 +1,25 @@
 class Solution:
-    def thousandSeparator(self, n: int) -> str:        
-        return ("".join([str(n)[::-1][i:i+3]+"." for i in range(0, len(str(n)), 3)]))[::-1][1:]
+    def thousandSeparator(self, n: int) -> str: 
+        return f"{n:,}".replace(',', '.')
+    
+"""
+One line:
+```python
+return "".join([str(n)[::-1][i:i+3]+"." for i in range(0, len(str(n)), 3)])[::-1][1:]
+```
+
+More readable:
+```python
+def thousandSeparator(n): 
+
+    reversed_str = str(n)[::-1]
+    chunk_size = 3
+    result = ""
+
+    for i in range(0, len(reversed_str), chunk_size):
+        result += reversed_str[i:i+chunk_size] + "."
+
+    return result[::-1][1:]
+```
+"""
         
