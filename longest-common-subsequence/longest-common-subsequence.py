e8d66b53666b33e47fa7acdcdd5da1b82f065174
Josh Arnold <dev@josharnold.me>
2021-08-08 14:17:46 -0700

    Time: 1012 ms (11.69%), Space: 144.7 MB (5.03%) - LeetHub

--- longest-common-subsequence/longest-common-subsequence.py
+++ longest-common-subsequence/longest-common-subsequence.py
@@ -1,23 +1,25 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:        
-            
-        @lru_cache(maxsize=None)
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
+        @functools.lru_cache(maxsize=None)
         def recurse(idx, jdx):
-            if idx == len(text1) or jdx == len(text2):
+            
+            if (
+                idx == len(text1)
+                or jdx == len(text2)           
+            ):
                 return 0
-            a, b = recurse(idx+1, jdx), 0
-            occ = text2.find(text1[idx], jdx)
-            if occ != -1:
-                b = 1 + recurse(idx+1, occ+1)
-            return max(a, b)
+            
+            option_a = recurse(idx+1, jdx)
+            
+            option_b = 0            
+            occurance = text2.find(text1[idx], jdx)
+            if occurance != -1:
+                option_b = 1 + recurse(idx+1, occurance+1)
+                
+            return max(
+                option_a,
+                option_b
+            )
         
         return recurse(0, 0)
-            
-
-        
-    
-    
-"""
-- what if we found the LCS of size one
-- then from LCS of 1, we use that as a building block for LCS of size 2?
-"""
