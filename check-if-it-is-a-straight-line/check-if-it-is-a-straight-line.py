dc21641e5e11a4b77bf064f60f33034d4aebf261
Josh Arnold <dev@josharnold.me>
2021-10-02 10:21:39 -0700

    Time: 1448 ms (5.03%), Space: 68 MB (13.57%) - LeetHub

---
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
@@ -0,0 +1,18 @@
+from functools import reduce
+
+class Solution:
+    def checkStraightLine(self, A: List[List[int]]) -> bool:
+        A.sort()
+        results = []
+        for i in A:
+            for j in A:
+                try:
+                    results.append(
+                        (i[1]-j[1]) / (i[0]-j[0])
+                    )
+                except:
+                    pass
+        return all([
+            i == results[0]
+            for i in results
+        ])
